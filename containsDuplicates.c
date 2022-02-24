9daa5f70d8d31f51b1b339d2820bfc4aa6e96503
royal-22 <87497586+royal-22@users.noreply.github.com>
2022-02-24 13:44:35 -0500

    Update containsDuplicates.c

--- containsDuplicates.c
+++ containsDuplicates.c
@@ -2,8 +2,10 @@ int sort(const void *p1, const void * p2){
     return (*(int*)p1 - *(int*)p2);
 }
 
+// qsort explained --> https://www.educative.io/edpresso/what-is-the-qsort-function-in-c
+
 bool containsDuplicate(int* nums, int numsSize){
-    
+// slow solution (not recommended)    
 /*   for (int n=0; n<numsSize; n++){
         for (int z = numsSize; z>n; z--){
             if (nums[n] == nums[z]){
